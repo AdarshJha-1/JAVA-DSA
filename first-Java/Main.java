612acc66f15283aa39e11db3fc218987b53bb9dd
AdarshJha <adarsh2005j@gmail.com>
2024-02-14 16:56:21 +0530

    learn to work with intellij -- Java :)

--- first-Java/Main.java
+++ first-Java/Main.java
@@ -1,5 +1,5 @@
-public class Main{ // access modifier
-    public static void main(String[] args){
-        System.out.println("Hello World!");
+public class Main { // access modifier
+    public static void main(String[] args) { // public means any file can access it
+        System.out.println("Hello World! "+ args[0]); // main() is a entry point of the code/program compulsory main()
     }
 }
